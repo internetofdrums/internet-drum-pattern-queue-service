a378efc0429e85a6ac2c8a0c34b9992af83e1e59
Piet van Dongen <piet.vandongen@luminis.eu>
2018-04-02 21:18:46 +0200

    Changed drum pattern length requirements according to new specs

--- com.internetofdrums.api.queue.service.api/src/main/java/com/internetofdrums/api/queue/service/api/DrumPatternData.java
+++ com.internetofdrums.api.queue.service.api/src/main/java/com/internetofdrums/api/queue/service/api/DrumPatternData.java
@@ -2,7 +2,7 @@ package com.internetofdrums.api.queue.service.api;
 
 public interface DrumPatternData {
 
-    int REQUIRED_PATTERN_LENGTH = 684;
+    int REQUIRED_PATTERN_LENGTH = 256;
 
     String getPattern();
 }
