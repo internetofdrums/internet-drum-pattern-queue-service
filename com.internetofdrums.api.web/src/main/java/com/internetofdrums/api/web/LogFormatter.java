b4f2981ef72d750b9d5a552bcb7887271de8e07d
Piet van Dongen <piet.vandongen@luminis.eu>
2018-08-10 21:25:12 +0200

    Added source information to logging

--- com.internetofdrums.api.web/src/main/java/com/internetofdrums/api/web/LogFormatter.java
+++ com.internetofdrums.api.web/src/main/java/com/internetofdrums/api/web/LogFormatter.java
@@ -3,31 +3,55 @@ package com.internetofdrums.api.web;
 import java.io.PrintWriter;
 import java.io.StringWriter;
 import java.time.Instant;
-import java.util.logging.Formatter;
 import java.util.logging.LogRecord;
+import java.util.logging.SimpleFormatter;
 
-public class LogFormatter extends Formatter {
+public class LogFormatter extends SimpleFormatter {
 
-    private static final String FORMAT = "%1$s %2$-7s %3$s %4$s%n";
+    private static final String FORMAT = "%1$s %4$-7s %2$s - %5$s %6$s%n";
+
+    private static String getDateTime(LogRecord logRecord) {
+        return Instant.ofEpochMilli(logRecord.getMillis()).toString();
+    }
+
+    private static String getSource(LogRecord logRecord) {
+        if (logRecord.getSourceClassName() == null) {
+            return logRecord.getLoggerName();
+        }
+
+        String source = logRecord.getSourceClassName();
+
+        if (logRecord.getSourceMethodName() != null) {
+            source += " " + logRecord.getSourceMethodName();
+        }
+
+        return source;
+    }
+
+    private static String getThrowable(LogRecord logRecord) {
+        if (logRecord.getThrown() == null) {
+            return "";
+        }
+
+        StringWriter stringWriter = new StringWriter().append(System.getProperty("line.separator"));
+        PrintWriter printWriter = new PrintWriter(stringWriter);
+
+        logRecord.getThrown().printStackTrace(printWriter);
+        printWriter.close();
+
+        return stringWriter.toString();
+    }
 
     @Override
     public String format(LogRecord logRecord) {
-        String stackTrace = "";
-        Throwable throwable = logRecord.getThrown();
-
-        if (throwable != null) {
-            StringWriter stringWriter = new StringWriter().append(System.getProperty("line.separator"));
-            throwable.printStackTrace(new PrintWriter(stringWriter));
-
-            stackTrace = stringWriter.toString();
-        }
-
         return String.format(
                 FORMAT,
-                Instant.ofEpochMilli(logRecord.getMillis()).toString(),
+                getDateTime(logRecord),
+                getSource(logRecord),
+                logRecord.getLoggerName(),
                 logRecord.getLevel().getName(),
                 formatMessage(logRecord),
-                stackTrace
+                getThrowable(logRecord)
         );
     }
 }
